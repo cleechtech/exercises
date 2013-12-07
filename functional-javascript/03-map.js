ace2c298c9b3126c1140b34d0e041dbf928a938c
Connor James Leech <connorleech@gmail.com>
2013-12-07 16:01:17 +0300

    so it begins

---
+++ functional-javascript/03-map.js
@@ -0,0 +1,33 @@
+/*
+Convert the following code from a for-loop to Array#map:
+Arguments: 
+	* numbers: An Array of 1 to 20 Integers between 0 and 9
+
+Conditions:
+	* Do not use any for/while loops.
+	* You do not need to define any additional functions
+	  unless a stub is provided in the boilerplate.
+	* Your solution should use Array.prototype.map()
+
+```
+function doubleAll(numbers) {
+  var result = []
+  for (var i = 0; i < numbers.length; i++) {
+    result.push(numbers[i] * 2)
+  }
+  return result
+}
+
+module.exports = doubleAll
+```
+
+Resources: 
+	* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+*/
+
+function doubleAll(numbers){
+	
+}
+
+module.exports = doubleAll;
