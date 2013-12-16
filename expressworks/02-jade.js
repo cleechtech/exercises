7ab24a6c1c73d2c2314399546ac14bde46692766
Connor James Leech <connorleech@gmail.com>
2013-12-16 19:18:29 +0300

    rabbitmq messing everything up

--- expressworks/02-jade.js
+++ expressworks/02-jade.js
@@ -1,4 +1,18 @@
+// Create an Express.js app with a home page (/home) 
+// rendered by jade template engine, that shows current date (toDateString).
+
 var express = require('express');
 var jade = require('jade');
-var http = require('http');
 var app = express();
+
+
+
+app.get('/home', function(req,res){
+	app.set('view engine', 'jade');
+    app.set('views', process.argv[3]);
+	res.render('index', {date: new Date().toDateString()});
+});
+
+
+
+
