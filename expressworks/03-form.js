ebac5fabd65c45bb0b7c87397d6a337e5a5b3c84
Connor James Leech <connorleech@gmail.com>
2013-12-17 10:43:06 +0300

    form

---
+++ expressworks/03-form.js
@@ -0,0 +1,13 @@
+// Write a route ('/form') that processes HTML form input 
+// (<form><input name="str"/></form>) and prints backwards the str value.
+var express = require('express')
+var connect = require('connect')
+var app = express()
+
+app.use(express.urlencoded())
+
+app.get('/form', function(req,res){
+	res.send(req.body.str.split('').reverse().join(''))
+})
+
+app.listen(process.argv[2])
