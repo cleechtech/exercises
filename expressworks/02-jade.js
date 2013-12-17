57b0feb3c7fa7c6afdb32375d27d3942cff89f49
Connor James Leech <connorleech@gmail.com>
2013-12-17 10:17:22 +0300

    jade

--- expressworks/02-jade.js
+++ expressworks/02-jade.js
@@ -5,14 +5,12 @@ var express = require('express');
 var jade = require('jade');
 var app = express();
 
-
+app.set('view engine', 'jade');
+app.set('views', process.argv[3]);
 
 app.get('/home', function(req,res){
-	app.set('view engine', 'jade');
-    app.set('views', process.argv[3]);
 	res.render('index', {date: new Date().toDateString()});
 });
 
-
-
+app.listen(process.argv[2]);
 
