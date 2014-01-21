804d1920d96b8d7b144c9c0156419cc44a88a04a
Connor James Leech <connorleech@gmail.com>
2014-01-21 13:47:25 +0300

    remove comment fail :)

--- filelist/editFile.js
+++ filelist/editFile.js
@@ -5,7 +5,6 @@ var fileList = 'filelist.txt'; // process.argv[1] reads first command line argum
 fs.readFile(fileList, 'utf-8', function(err, data){
 	if (err) throw err;
 	
-	// only removes '.png' from the first line..
 	var newValue = data.replace(/^\./gm, 'www.youtube.com').replace(/.png/gm, '');
 	
 	fs.writeFile('filelist2.txt', newValue, function(err){
