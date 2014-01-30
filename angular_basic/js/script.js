283402cf3e1dd7595d493d5858c85a02366bb791
Connor James Leech <connorleech@gmail.com>
2014-01-30 15:45:15 +0300

    comment notes for future

--- angular_basic/js/script.js
+++ angular_basic/js/script.js
@@ -32,3 +32,6 @@ angular_basic
 		$scope.message = 'this is a dummy contact page';
 	});
 
+// share data between controllers: http://www.thinkster.io/pick/9jfpSmbx1j/angularjs-sharing-data-between-controllers
+// define a method on the scope (in a controller): http://www.thinkster.io/pick/S6NUuKBgDW/angularjs-defining-a-method-on-the-scope
+// understand isolate scope (ex: children do chores): http://www.thinkster.io/pick/KnxWvHUW64/angularjs-understanding-isolate-scope
