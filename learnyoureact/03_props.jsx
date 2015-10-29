86b757e3d244c2d2ea2b94b0a1d1b438d74f0ceb
Connor Leech <cleech@proteus.com>
2015-10-29 10:38:17 -0700

    03 props

--- learnyoureact/03_props.jsx
+++ learnyoureact/03_props.jsx
@@ -1,15 +1,9 @@
+// pass values from a parent component to a child component with props
+
 var React = require('react');
     
 var TodoBox = React.createClass({
-  render: function() {
-    return (
-      <div className="todoBox">
-        <h1>Todos</h1>
-        <TodoList />
-        <TodoForm />
-      </div>
-    );
-  }
+  // Omitted
 });
 
 var TodoList = React.createClass({
@@ -31,6 +25,7 @@ var Todo = React.createClass({
   // {this.props.title} == value passed to title attribute
   // {this.props.children} == innerHTML
 
+  // inline styles must be set with javascript objects
   render: function(){
     var todoStyles = {
       title: {
@@ -50,13 +45,26 @@ var Todo = React.createClass({
 });
 
 var TodoForm = React.createClass({
-  render: function(){
-  	return (
-  		<div className= "todoForm">
-    		I am a TodoForm.
-    	</div>
-  	);
-  }
+  // Omitted
 });
 
 module.exports = TodoBox;
+
+// Within Todo, you can get the value of the title attribute set in TodoList
+// (the parent component) by using {this.props.title}. Likewise, you can get the
+// values Milk and 13:00 by using {this.props.children}.
+
+// <div class="todoList">
+//   <table style="border:2px solid black;">
+//     <tbody>
+//       <tr>
+//         <td style="border:1px solid black;">Shopping</td>
+//         <td style="border:1px solid black;">Milk</td>
+//       </tr>
+//       <tr>
+//         <td style="border:1px solid black;">Hair cut</td>
+//         <td style="border:1px solid black;">13:00</td>
+//       </tr>
+//     </tbody>
+//   </table>
+// </div>
